3a69277689474696510ea072e8aa1ac273764414
Diego Berrocal <cestdiego@gmail.com>
2015-05-29 01:20:52 -0500

    yeah

--- org-cestdiego/org-page-themes/just_right/resources/js/main.js
+++ org-cestdiego/org-page-themes/just_right/resources/js/main.js
@@ -1,38 +1,119 @@
-$(document).ready(function() {
-    /*******************************************************************
-     * 1. replace css class "src" and "example" with "prettyprint", for
-     *    prettify.js to render
-     * 2. replace detail language css class, e.g. "src-scheme" to
-     *    "lang-scheme" per the description of prettify.js
-     ******************************************************************/
-    var $blocks = $('pre.src');
-    $blocks.each(function(index) {
-        var self = $(this);
-        var classes = self.removeClass('src').attr('class').split(/\s+/);
-        $.each(classes, function(idx, cls) {
-            if (cls.substring(0, 4) === 'src-') {
-                var lang = cls.substring(4);
-                self.removeClass(cls).addClass('lang-' + lang);
-            }
+
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+
+// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+
+// MIT license
+
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+// Taken from http://stackoverflow.com/questions/8917921/cross-browser-javascript-not-jquery-scroll-to-top-animation
+
+//t = current time
+//b = from value
+//c = change in value
+//d = duration
+var easeInOutQuad = function (t, b, c, d) {
+    t /= d/2;
+    if (t < 1) return c/2*t*t + b;
+    t--;
+    return -c/2 * (t*(t-2) - 1) + b;
+};
+
+function scroll(from, to, duration) {
+    var change = to - from,
+        startTime,
+        currentTime = 0;
+
+    var setStartTime = function(timestamp) {
+        startTime = timestamp;
+        requestAnimationFrame(animateScroll);
+    };
+
+    var animateScroll = function(timestamp) {
+        currentTime = timestamp - startTime;
+        document.documentElement.scrollTop = document.body.scrollTop = easeInOutQuad(currentTime, from, change, duration);
+        if(currentTime <= duration) {
+            requestAnimationFrame(animateScroll);
+        }
+    };
+
+    requestAnimationFrame(setStartTime);
+}
+
+(function() {
+    var lightTheme = document.querySelector("#lightTheme");
+    var darkTheme = document.querySelector("#darkTheme");
+
+    var toggleTheme = function () {
+        var isDefaultDisabled = lightTheme.disabled;
+        darkTheme.disabled = isDefaultDisabled;
+        lightTheme.disabled = !isDefaultDisabled;
+
+        var switchLights = document.querySelector(".switch-lights");
+        switchLights.textContent = "lights " + (isDefaultDisabled ? "off" : "on");
+
+        return !isDefaultDisabled;
+    };
+
+    if (localStorage.darkTheme === "1") {
+        toggleTheme();
+    }
+
+    var viewportHeight = document.documentElement.clientHeight,
+        documentHeight = document.body.clientHeight,
+        backToTop;
+
+    if (documentHeight < viewportHeight) {
+        backToTop = document.querySelector(".back-to-top");
+        backToTop.style.display = "none";
+    }
+
+    window.addEventListener("load", function () {
+        document.querySelector(".switch-lights").addEventListener("click", function () {
+            localStorage.darkTheme = +toggleTheme();
         });
-        self.addClass('prettyprint');
-    });
-    $('pre.example').removeClass('example').addClass('prettyprint');
 
-    /*******************************************************************
-     * 1. remove all org exported line number spans
-     * 2. add css class "linenums" to code block per the description of
-     *    prettify.js
-     ******************************************************************/
-    var $lines = $('span.linenr');
-    var $linedBlocks = $lines.parent();
-    $lines.remove();
-    $linedBlocks.each(function(index) {
-        $(this).addClass('linenums');
-    });
+        Array.prototype.forEach.call(document.querySelectorAll('a[href^="#"]'), function (el) {
+            el.addEventListener("click", function (event) {
+                if (!(location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') &&
+                      location.hostname == this.hostname)) {
+                    return;
+                }
 
-    /*******************************************************************
-     * pretty print all code blocks
-     ******************************************************************/
-    prettyPrint();
-});
+                var to;
+                if (this.hash === "") {
+                    to = 0;
+                } else {
+                    to = document.querySelector(this.hash).scrollTop;
+                }
+                scroll(document.documentElement.scrollTop || document.body.scrollTop, to, 600);
+                event.preventDefault();
+            });
+        });
+    });
+}());

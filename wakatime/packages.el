9fe526a85add6b76c18664ea501618f76e27bd1f
Diego Berrocal <cestdiego@gmail.com>
2015-06-01 12:20:33 -0500

    Added wakatime package

--- wakatime/packages.el
+++ wakatime/packages.el
@@ -3,10 +3,9 @@
     wakatime-mode
     ))
 
-(defun wakatime/init-wakatime ()
+(defun wakatime/init-wakatime-mode ()
   (use-package wakatime-mode
-    :defer t
-    :init
-    (global-wakatime-mode)
     :config
-    (setq wakatime-api-key "813b0d78-1f17-43eb-bede-a5c008651d4a")))
+    (setq wakatime-cli-path "/home/io/.local/bin/wakatime")
+    (setq wakatime-api-key "813b0d78-1f17-43eb-bede-a5c008651d4a")
+    (global-wakatime-mode)))

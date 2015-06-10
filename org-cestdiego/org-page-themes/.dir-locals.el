5fb30e518a36cc24ce4ac3c388cd244b4ea9d4d2
Diego Berrocal <cestdiego@gmail.com>
2015-06-09 23:47:35 -0500

    Moved org-page-themes

--- org-cestdiego/org-page-themes/.dir-locals.el
+++
@@ -1,5 +0,0 @@
-((nil . ((firestarter . (op/do-publication
-                         t
-                         t
-                         "~/Projects/le_blog_built"))))
- ("just_right/.git" . ((firestarter . nil))))

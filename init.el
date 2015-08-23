c60ec97fe36511ac305a795463f4ff7f3297ed61
Diego Berrocal <cestdiego@gmail.com>
2015-08-23 15:53:24 -0500

    kek

--- init.el
+++ init.el
@@ -136,8 +136,8 @@ before layers configuration."
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
-                         monokai
                          aurora
+                         monokai
                          spacemacs-dark
                          zenburn ;; This works without erc
                          spacemacs-light

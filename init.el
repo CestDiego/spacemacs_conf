2a1965e1fb06b3d6aaa1eac0117d99ef35b44699
Diego Berrocal <cestdiego@gmail.com>
2015-08-24 19:47:15 -0500

    Change OS Theme along with emacs theme

--- init.el
+++ init.el
@@ -282,6 +282,27 @@ layers configuration."
 
   (add-hook 'visual-line-mode-hook 'visual-fill-column-mode)
 
+  (defadvice spacemacs/post-theme-init (after set-colors-as-env-variables activate)
+    "Change Window Manager Theme as well! :O"
+    (setenv "BACKGROUND"     (cdr (aref ansi-color-map 40)))
+    (setenv "FOREGROUND"     (cdr (aref ansi-color-map 37)))
+    (setenv "LIGHT_GREY"     (cdr (aref ansi-color-map 30)))
+    (setenv "LIGHT_RED"      (cdr (aref ansi-color-map 32)))
+    (setenv "LIGHT_GREEN"    (cdr (aref ansi-color-map 33)))
+    (setenv "LIGHT_BLUE"     (cdr (aref ansi-color-map 34)))
+    (setenv "LIGHT_MAGENTA"  (cdr (aref ansi-color-map 35)))
+    (setenv "LIGHT_CYAN"     (cdr (aref ansi-color-map 36)))
+    (setenv "LIGHT_GREY"     (cdr (aref ansi-color-map 37)))
+    (setenv "DARK_RED"       (cdr (aref ansi-color-map 41)))
+    (setenv "DARK_GREEN"     (cdr (aref ansi-color-map 42)))
+    (setenv "DARK_YELLOW"    (cdr (aref ansi-color-map 43)))
+    (setenv "DARK_BLUE"      (cdr (aref ansi-color-map 44)))
+    (setenv "DARK_MAGENTA"   (cdr (aref ansi-color-map 45)))
+    (setenv "DARK_CYAN"      (cdr (aref ansi-color-map 46)))
+    (setenv "DARK_GREY"      (cdr (aref ansi-color-map 47)))
+    (when (not (string= (getenv "BACKGROUND") "black"))
+      (message (shell-command-to-string "change-theme"))))
+
   ;; Use helm-projectile anywhere
   ;; (setq projectile-require-project-root nil)
 

429c347e2ff45967692d47637d9dda685d61b39e
Diego Berrocal <cestdiego@gmail.com>
2015-09-29 17:53:39 -0400

    don't use fork for now

--- utils/packages.el
+++ utils/packages.el
@@ -25,7 +25,7 @@
     wakatime-mode
     ;; nameless
     keyfreq
-    (nix-mode :location local)
+    ;; (nix-mode :location local)
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")

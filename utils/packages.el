e64d30a10a7e2c5f9156b7ee6a919ea344ea1d54
Diego Berrocal <cestdiego@gmail.com>
2015-12-23 01:15:42 -0500

    Add a better helm-http-status

--- utils/packages.el
+++ utils/packages.el
@@ -61,18 +61,31 @@ which require an initialization must be listed explicitly in the list.")
 
 (defun utils/post-init-helm ()
   ;; Helm command to display HTTP status codes.
-  (defvar helm-httpstatus-source
-    '((name . "HTTP STATUS")
-      (candidates . (("100 Continue") ("101 Switching Protocols")
-                     ("102 Processing") ("200 OK")
+  (defvar helm-http-1xx-status-source
+    '((name . "Informational 1xx")
+      (candidates . (("100 Continue") ("101 Switching Protocols") ("102 Processing")))
+      (action . message)))
+
+  (defvar helm-http-2xx-status-source
+    '((name . "Successful 2xx")
+      (candidates . (("200 OK")
                      ("201 Created") ("202 Accepted")
                      ("203 Non-Authoritative Information") ("204 No Content")
                      ("205 Reset Content") ("206 Partial Content")
-                     ("207 Multi-Status") ("208 Already Reported")
-                     ("300 Multiple Choices") ("301 Moved Permanently")
+                     ("207 Multi-Status") ("208 Already Reported")))
+      (action . message)))
+
+  (defvar helm-http-3xx-status-source
+    '((name . "Redirection 3xx")
+      (candidates . (("300 Multiple Choices") ("301 Moved Permanently")
                      ("302 Found") ("303 See Other")
                      ("304 Not Modified") ("305 Use Proxy")
-                     ("307 Temporary Redirect") ("400 Bad Request")
+                     ("307 Temporary Redirect")))
+      (action . message)))
+
+  (defvar helm-http-4xx-status-source
+    '((name . "Client Error 4xx")
+      (candidates . (("400 Bad Request")
                      ("401 Unauthorized") ("402 Payment Required")
                      ("403 Forbidden") ("404 Not Found")
                      ("405 Method Not Allowed") ("406 Not Acceptable")
@@ -89,22 +102,32 @@ which require an initialization must be listed explicitly in the list.")
                      ("426 Upgrade Required") ("428 Precondition Required")
                      ("429 Too Many Requests")
                      ("431 Request Header Fields Too Large")
-                     ("449 Retry with") ("500 Internal Server Error")
-                     ("501 Not Implemented") ("502 Bad Gateway")
+                     ("449 Retry with")))
+      (action . message)))
+
+  (defvar helm-http-5xx-status-source
+    '((name . "Server Error 5xx")
+      (candidates . (("500 Internal Server Error") ("501 Not Implemented")
+                     ("502 Bad Gateway")
                      ("503 Service Unavailable") ("504 Gateway Timeout")
                      ("505 HTTP Version Not Supported")
-                     ("506 Variant Also Negotiates")
-                     ("507 Insufficient Storage") ("509 Bandwidth Limit Exceeded")
+                     ("506 Variant Also Negotiates") ("507 Insufficient Storage") ("509 Bandwidth Limit Exceeded")
                      ("510 Not Extended")
                      ("511 Network Authentication Required")))
       (action . message)))
 
+
   (defun helm-httpstatus ()
     (interactive)
-    (helm-other-buffer '(helm-httpstatus-source) "*helm httpstatus*"))
+    (helm :sources (list helm-http-1xx-status-source
+                         helm-http-2xx-status-source
+                         helm-http-3xx-status-source
+                         helm-http-4xx-status-source
+                         helm-http-5xx-status-source)))
   (spacemacs/set-leader-keys
     "hh" 'helm-httpstatus)
-      )
+  )
+
 
 (defun utils/init-fontawesome ()
   (use-package fontawesome

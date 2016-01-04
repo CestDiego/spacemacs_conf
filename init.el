5d7cd1e89913e0a4569700fb05a5f52b529654af
Diego Berrocal <cestdiego@gmail.com>
2016-01-04 15:34:53 -0500

    monokai is default again

--- init.el
+++ init.el
@@ -178,9 +178,9 @@ values."
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
+                         monokai
                          spacemacs-dark
                          tao-yin
-                         monokai
                          tao-yang
                          aurora
                          zenburn ;; This works without erc

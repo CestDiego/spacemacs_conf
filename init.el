9e7992c46b96852246f160e1c0905b7d8657de76
Diego Berrocal <cestdiego@gmail.com>
2016-10-16 11:58:23 -0400

    Who needs help?

--- init.el
+++ init.el
@@ -410,6 +410,8 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
 
+  (keyboard-translate ?\C-h ?\C-?)
+
   ;; (defun diego/transpile (ast)
   ;;   (cond ((numberp ast) ast)
   ;;         ((equal ast nil) nil)

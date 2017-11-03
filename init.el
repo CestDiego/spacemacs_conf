8f130f4fa166b140e0fa34940aaf9183dbe5acee
Diego Berrocal <cestdiego@gmail.com>
2017-11-03 10:54:56 -0400

    Add material theme

--- init.el
+++ init.el
@@ -212,6 +212,7 @@ It should only modify the values of Spacemacs settings."
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
+                         material
                          spacemacs-dark
                          monokai
                          zenburn

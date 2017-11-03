8e69e11241b00e4905f6c630b3de4b7c9e35af38
Diego Berrocal <cestdiego@gmail.com>
2017-11-03 10:55:02 -0400

    Add a better font

--- init.el
+++ init.el
@@ -223,7 +223,7 @@ It should only modify the values of Spacemacs settings."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Meslo LG M DZ for Powerline"
+   dotspacemacs-default-font '("Meslo LG L DZ for Powerline"
                                :size 16
                                :weight normal
                                :width normal

4473274e183779c28a425f7a0ae4cf08bf3313a0
Diego Berrocal <cestdiego@gmail.com>
2018-06-04 12:50:18 -0400

    Try to not distinguis gui tab and terminal tab

--- init.el
+++ init.el
@@ -327,7 +327,8 @@ It should only modify the values of Spacemacs settings."
    ;; and TAB or `C-m' and `RET'.
    ;; In the terminal, these pairs are generally indistinguishable, so this only
    ;; works in the GUI. (default nil)
-   dotspacemacs-distinguish-gui-tab t
+   dotspacemacs-distinguish-gui-tab nil
+
    ;; If non-nil `Y' is remapped to `y$' in Evil states. (default nil)
    dotspacemacs-remap-Y-to-y$ t
    ;; If non-nil, the shift mappings `<' and `>' retain visual state if used
